3759b6df69c54b8356b8ac03430ed0296059fa07
Lautaro <lautarolagosleiva@hotmail.com>
2020-09-11 00:57:45 -0300

    Verificacion de la factura
    Verifica si la fecha ingresa no es una fecha erronea, se implementa CSS tambien

--- Practico 4/add-bill.php
+++ Practico 4/add-bill.php
@@ -1,10 +1,10 @@
-
+<?php require 'header.php'; ?>
 <main class="py-5">
      <section id="listado" class="mb-5">
           <div class="container">
                <h2 class="mb-4">Agregar Factura</h2>
 
-               <form action="bill-check" method="get" class="bg-light-alpha p-5">
+               <form action="bill-check.php" method="GET" class="bg-light-alpha p-5">
                     <div class="row">
                          <div class="col-lg-3">
                               <div class="form-group">
@@ -15,19 +15,20 @@
                          <div class="col-lg-3">
                               <div class="form-group">
                                    <p>Tipo</p>
-                                   <input type="radio" name="factura" class="radioSize">Factura A
-                                   <input type="radio" name="factura" class="radioSize">Factura B
+                                   <input type="radio" name="factura" id="1" class="radioSize">Factura A
+                                   <input type="radio" name="factura" id="2" class="radioSize">Factura B
                               </div>
                          </div>
                          <div class="col-lg-3">
                               <div class="form-group">
                                    <label for="">Numero</label>
-                                   <input type="number" name="" class="form-control" min="0">
+                                   <input type="number" name="numero" class="form-control" min="0">
                               </div>
                          </div>
                     </div>
-                    <button type="button" name="button" class="btn btn-dark ml-auto d-block">Agregar</button>
+                    <button type="submit" name="button" class="btn btn-dark ml-auto d-block">Agregar</button>
                </form>
           </div>
      </section>
 </main>
+<?php require 'footer.php'; ?>
--- Practico 4/bill-check.php
+++ Practico 4/bill-check.php
@@ -1,3 +1,42 @@
 <?php
+    require_once "models/Item.php";
+    use models\Item as Item;
+
+   //Esto tira error por algun motivo, investigar
+    /* $arrayItems = array();
+
+    $Items = "pincel fino de 2/3, pincel de cerdas finas para acuarela, 120.00, 6,
+    pintura fluor 1L, pintura warner fluo, 400.00, 3,
+    plato de mezcla, plato plástico de mezcla con refuerzo anti caída, 200.00, 1,
+    pincel común 1,2, pincel fabber cerda común para tempera, 120.00, 5,
+    rodillo grueso 3/4, rodillo rugoso de expesor para exterior, 95.00, 2,
+    kit de acuarelas, combo de acuarelas color pastel, 770.00, 2";
+
+    $newArray = explode(',', $Items);
+
+    while(!empty($newArray))
+    {
+        $Item = new Item();
+        $Item->setName(array_shift($newArray));
+        $Item->setDescripcion(array_shift($newArray));
+        $Item->setPrice(array_shift($newArray));
+        $Item->setQuality(array_shift($newArray));
+        array_push($arrayItems, $Item);
+    }*/
     
+    if(isset($_GET))
+    {
+        $time = time();
+        $actualDate = date("Y-m-d", $time);
+        $dateIncome = $_GET['date'];
+
+        if($dateIncome < $actualDate)
+        {
+            header("location:bill-content.php");
+        }
+        else
+        {
+            header("location:add-bill.php");
+        }
+    }
 ?>
